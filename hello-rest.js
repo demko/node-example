64c4df255147052589201d083232c25dda75d585
demko <demko@tiscali.it>
2016-05-03 11:14:17 +0100

    Update hello-rest.js

--- hello-rest.js
+++ hello-rest.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from the fabric8 NodeJS example');
+  res.end('Hello from the fabric8 NodeJS example v2');
 }).listen(8080);
 console.log('Server running at http://localhost:8080/');
